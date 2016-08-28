0de046e2002dd6a94fe3745be7629c8213d1466d
Carles Pina i Estany <carles@pina.cat>
2016-08-28 16:57:34 +0200

    Cleans up variable names and formatting.
    (testing PyCharm)

--- map_generator/generate_data_map.py
+++ map_generator/generate_data_map.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import smell_hits
-import pprint
 import csv
-import sys
 import os
+
 
 class LocationFinder:
     def __init__(self):
@@ -15,35 +14,38 @@ class LocationFinder:
         cwd = os.path.dirname(__file__)
         borough_data_path = os.path.join(cwd, "..", "generate_boroughs", "all_borough_data.csv")
         print("Reading {} locations file".format(borough_data_path))
-        locationsfile = open(borough_data_path, "r", newline="")
+        locations_file = open(borough_data_path, "r", newline="")
 
-        locationsreader = csv.reader(locationsfile)
+        locations_reader = csv.reader(locations_file)
 
-        for line in locationsreader:
+        for line in locations_reader:
             self._locations[line[3]] = (line[0], line[1])
+            print("test locations_reader")
 
         print("Read {} different locations".format(len(self._locations)))
 
     def lookup(self, name):
         return self._locations[name]
 
+
 def create_carto_file():
     """ Creates a .csv file to be uploaded into http://carto.com with the smells. """
 
-    locationFinder = LocationFinder()
+    location_finder = LocationFinder()
 
     filename = "carto.csv"
-    csvfile=open(filename, "w", newline="")
+    csv_file = open(filename, "w", newline="")
     print("Output file: {}".format(filename))
 
-    mapwriter = csv.writer(csvfile, delimiter=",", quoting=csv.QUOTE_ALL)
+    map_writer = csv.writer(csv_file, delimiter=",", quoting=csv.QUOTE_ALL)
 
-    mapwriter.writerow(["longitude", "latitude", "year", "location_name", "number_of_smells"])
+    map_writer.writerow(["longitude", "latitude", "year", "location_name", "number_of_smells"])
 
     for borough_name, borough_information in smell_hits.res.items():
         for year, number_of_smells in borough_information.items():
-            (latitude, longitude) = locationFinder.lookup(borough_name)
-            mapwriter.writerow([latitude, longitude, year, borough_name, number_of_smells])
+            (latitude, longitude) = location_finder.lookup(borough_name)
+            map_writer.writerow([latitude, longitude, year, borough_name, number_of_smells])
+
 
 if __name__ == "__main__":
     create_carto_file()
