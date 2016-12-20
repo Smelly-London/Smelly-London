1feddc4fe21883c1438c55e71192ac2da22b92eb
Deborah Leem <violin1208@gmail.com>
2016-12-20 17:39:33 +0000

    Deleted broken file

--- NLTK_textmine/create_datasets.py
+++
@@ -1,5 +0,0 @@
-Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
