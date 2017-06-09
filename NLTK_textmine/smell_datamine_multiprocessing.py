36e1a128ae5199d3259554c61c5e01eb5aface41
Deborah Leem <violin1208@gmail.com>
2017-06-09 19:15:59 +0100

    to add time progress bar to the datamine multiprocessing script

--- NLTK_textmine/smell_datamine_multiprocessing.py
+++ NLTK_textmine/smell_datamine_multiprocessing.py
@@ -4,6 +4,7 @@ categorises by smell types. Only nltk sentence tokenizer is used.
 SQLite set up.
 '''
 
+import progressbar
 from map2 import mapping
 import concurrent.futures
 from timeit import default_timer as timer
@@ -18,7 +19,7 @@ from os import listdir
 import nltk.data
 import dataset
 
-SMELL_WORDS = ['smell', 'stench', 'stink', 'odour', 'sniff', 'effluvium', 'effluvia']
+SMELL_WORDS = ['smell', 'stench', 'stink', 'odour', 'sniff', 'effluvium', 'aroma', 'pungent', 'pungency']
 REPORTS_DIR = '../Full text'
 
 
@@ -46,28 +47,28 @@ class Smell(object):
         return repr(self) == repr(other)
 
 # TEMPLATE: category = SmellType('category_name', ['synonym1', 'synonym2'])
-# TODO: Create smell categories here
-sewer = SmellType('sewer', ['sewer', 'drain', 'sewage', 'manhole', 'gully', 'gulley', 'gullies', 'cesspool', 'ventilator', 'ventilation'])
+# Smell categories here
+sewer = SmellType('sewer', ['sewer', 'drain', 'sewage', 'manhole', 'gully', 'gulley', 'cesspool', 'ventilator', 'ventilation'])
 thames = SmellType('thames', ['quay', 'sediment', 'thames', 'river'])
 water = SmellType('water', ['water'])
 waste_rubbish = SmellType('waste-rubbish', ['refuse', 'dust', 'waste', 'dump', 'rubbish', 'offensive matter'])
-waste_excrement = SmellType('waste-excrement', ['excrement', 'excreta', 'privy', 'privies', 'manure', 'dung'])
-food = SmellType('food', ['food', 'yeast', 'pie', 'sauce', 'lemonade', 'bread', 'onion', 'vinegar', 'cherries', 'cherry', 'flavour', 'coffee', 'chocolate', 'cream', 'fruit', 'vegetable', 'salad', 'cheese',
-                          'pickle', 'gherkin', 'fish', 'kipper', 'fillet', 'steak', 'mutton', 'tripe', 'cake', 'milk',
-                          'yoghurt', 'butter', 'icing', 'caramel', 'canned', 'egg', 'preserve', 'cooking', 'veal',
+waste_excrement = SmellType('waste-excrement', ['excrement', 'excreta', 'privy', 'manure', 'dung'])
+food = SmellType('food', ['food', 'stock', 'yeast', 'pie', 'sauce', 'lemonade', 'bread', 'onion', 'vinegar', 'cherry', 'flavour', 'coffee', 'chocolate', 'cream', 'fruit', 'vegetable', 'salad', 'cheese',
+                          'pickle', 'tea', 'gherkin', 'fish', 'kipper', 'fillet', 'steak', 'mutton', 'tripe', 'cake', 'milk',
+                          'yoghurt', 'butter', 'ice', 'caramel', 'can', 'egg', 'preserve', 'cook', 'veal',
                           'lamb', 'soup', 'peel', 'ham', 'sausage', 'cow', 'meat', 'sour', 'beef', 'rice', 'trough'])
 
-trade = SmellType('trade', ['trade', 'business', 'laboratory', 'laboratories', 'copper', 'dry cleaning', 'launderette',
+trade = SmellType('trade', ['trade', 'business', 'laboratory', 'copper', 'dry cleaning', 'launderette',
                             'laundrette', 'chemist', 'hide', 'bladder', 'glue', 'tannery', 'tanneries', 'rubber', 'gum', 'fat', 'oil', 'fellmonger', 'slaughter',
                             'costermonger', 'manure manufacture', 'ferment', 'butcher', 'burning'])
 animal = SmellType('animal', ['animal', 'pig', 'stable', 'piggeries', 'piggery', 'manure', 'excrement', 'cowhouse'])
 disinfectant = SmellType('disinfectant', ['disinfect', 'antiseptic'])
 factory_fuel = SmellType('factory-fuel', ['factory', 'factories', 'industrial', 'rubber', 'naphtha', 'fuel', 'works'])
-school = SmellType('school', ['school', 'lavatories', 'lavatory', 'discharging ears', 'playground'])
+school = SmellType('school', ['school', 'lavatories', 'lavatory', 'discharge', 'playground'])
 air = SmellType('air', ['gas', 'air', 'atmosphere', 'coal', 'carbonic acid', 'hydrogen', 'vapour', 'smoke', 'sulphide'])
 decomposition = SmellType('decomposition', ['mortuary', 'coffin', 'decomposition', 'burial', 'dead', 'body', 'church', 'chapel'])
-habitation = SmellType('habitation', ['house', 'premise' 'flat', 'dwelling', 'cottage', 'room', 'home', 'ward', 'clothing', 'bedding', 'barge', 'cupola'])
-absence_of_smell = SmellType('absence of smell', ['no offensive smell', 'no effluvium', 'smell-none', 'no smell', 'no nuisance from smell', 'absence of smell', 'no offensive odour', 'no bad odour', 'odourless', 'no disagreeable smell'])
+habitation = SmellType('habitation', ['house', 'premise' 'flat', 'dwell', 'cottage', 'room', 'home', 'ward', 'clothing', 'bed', 'barge', 'cupola'])
+absence_of_smell = SmellType('absence of smell', ['no offensive smell', 'no effluvium', 'smell-none', 'no smell', 'no nuisance from smell', 'absence of smell', 'no offensive odour', 'no bad odour', 'odourless', 'no disagreeable smell', 'devoid of aroma', 'no aroma','deficient in aroma', 'deficient of aroma'])
 
 
 def get_file_names():
@@ -75,13 +76,53 @@ def get_file_names():
     fileNames = [f for f in listdir(REPORTS_DIR) if f.endswith('txt')]
     return fileNames
 
+def get_new_pos(old_pos):
+    """This is a function that converts part-of-speech codes to abbreviated parts-of-speech"""
+    new_pos = ""
+    if old_pos.startswith('J'):
+        new_pos = "a"
+    elif old_pos.startswith('V'):
+        new_pos = "v"
+    elif old_pos.startswith('N'):
+        new_pos = "n"
+    elif old_pos.startswith('R'):
+        new_pos = "r"
+    else:
+        new_pos = ""
 
-def tokenize_to_sentence(sentence):
+    return new_pos
+
+def lemmatize_sentence(sentence):
+    """This is a fuction that takes a sentence and lemmatize the sentence."""
+    lemmatized = []
+    tokens = nltk.word_tokenize(sentence)
+    pos_tagging = nltk.pos_tag(tokens)
+
+    wnl = nltk.WordNetLemmatizer()
+
+    for (word, pos) in pos_tagging:
+        wordnet_pos = get_new_pos(pos)
+        if wordnet_pos != "":
+            lemma = wnl.lemmatize(word.lower(), wordnet_pos)
+        else:
+            lemma = word.lower()
+
+        if word.istitle():
+            lemma = lemma.capitalize()
+        elif word.upper() == word:
+            lemma = lemma.upper()
+
+        # if word != lemma:
+        #     print((word, lemma, pos, wordnet_pos))
+        lemmatized.append(lemma)
+
+    return " ".join(lemmatized)
+
+def tokenize_to_sentence(text):
     parser = nltk.data.load('tokenizers/punkt/english.pickle')
     # split into sentences
-    result = parser.tokenize(sentence.strip())
-    return result
-
+    sentences = parser.tokenize(text.strip())
+    return [lemmatize_sentence(sentence) for sentence in sentences]
 
 def worker(file_name):
     """This is a function used to calculate the result in concurrent futures."""
@@ -162,6 +203,7 @@ class SmellDataMine(object):
                                 break
 
     def categorise_sentence(self, sentence):
+        """ This is a function that categorises"""
         categories = []
         for category in self.smellTypes:
             for synonym in category.synonyms:
@@ -176,9 +218,13 @@ def main():
     files = get_file_names()
     smell_results = []
 
+    bar = progressbar.ProgressBar(max_value=len(files))
+    processed_files = 0
     with concurrent.futures.ProcessPoolExecutor() as executor:
         for file, smell in zip(files, executor.map(worker, files)):
             smell_results = smell_results + smell
+            processed_files += 1
+            bar.update(processed_files)
     smell_results = [x for x in smell_results if x]
 
     end = timer()
