81c447b730f1850af36906a7a8175bd87d0fc1ed
Deborah Leem <violin1208@gmail.com>
2017-06-18 08:59:06 +0100

    to add updated xml parser

--- import_parse_xml_data.py
+++ import_parse_xml_data.py
@@ -1,53 +1,35 @@
+"""
+To go through each XML table file and look for Diphtheria
+
+"""
+
 from xml.etree import ElementTree as ET
 from xml.etree.ElementTree import XMLParser
+from glob import glob
+from pprint import pprint
 
-filename = '/Users/deborah/Documents/scripts/python_work/project2016/MOH tables.xml/1920-1929.xml/Acton.1920.b19783504.xml/10197835000540034.xml'
-#import pdb; pdb.set_trace()
+def get_diphteria_data():
+	files = glob('/Users/deborah/Documents/scripts/python_work/project2016/MOH tables.xml/*/*/*.xml')
+	for file in files:
+		try:
+			parser = XMLParser(encoding="windows-1252")
+			root = ET.parse(file, parser=parser).getroot()
+		except:
+			# Just ignore bad files
+			pass
+		rows = [[(e.text.strip() if e.text else "") for e in row.findall("td")] for row in root.findall("*//table/tbody/tr")]
+		diphtheria_rows = [row for row in rows if "diphtheria" in " ".join(row).lower()]
+		if diphtheria_rows:
+			#print(file)
+			data = {
+				"publisher_loc": root.find("*//publisher-loc").text,
+				"year": root.find("*//pub-date/year").text,
+				"label": root.find("*//label").text,
+				"title": root.find("*//book-title").text,
+				"table_headers": [(e.text.strip() if e.text else "") for e in root.findall("*//table/thead/tr/th")],
+				"table_contents": diphtheria_rows,
+				"file": file,
+			}
+			pprint(data)
 
-parser = XMLParser(encoding="windows-1252")
-root = ET.parse(filename, parser=parser).getroot()
-#print(root)
-#calling dir(root) to review the root object's methods
-#print(dir(root))
-
-#a list of Element objects: 'book', 'book-meta' & 'book-front'. 'book-front' contains the information I need.
-#print(list(root))
-
-#to get all 'table-wrap' children
-data = root.findall('./book-front/table-wrap/*')
-#print(data)
-
-print(root.find('*//publisher-loc').text)
-print(root.find('*//object-id').text)
-print(root.find('*//label').text)
-print(root.find('*//caption/p').text)
-
-print([e.text for e in root.findall('*//table/thead/tr/th')])
-print([e.text for e in root.findall('*//table/tbody/tr/td')])
-
-
-
-
-
-
-# data = root.find('Data')
-
-# all_data = []
-
-# for table in data:
-#     record = {}
-#     for item in table:
-
-#         lookup_key = list(item.attrib.keys())[0]
-
-#         if lookup_key == 'Numeric':
-#             rec_key = 'NUMERIC'
-#             rec_value = item.attrib['Numeric']
-#         else:
-#             rec_key = item.attrib[lookup_key]
-#             rec_value = item.attrib['Code']
-
-#         record[rec_key] = rec_value
-#     all_data.append(record)
-
-# print all_data
+get_diphteria_data()
